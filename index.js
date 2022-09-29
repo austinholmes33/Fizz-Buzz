897cbe35f1b12f19af5a49aac23cb740c67f0805
austinholmes33 <>
2022-09-29 16:33:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 50; i++) {
+      console.log(i + 1)
+        if (i % 5 === 0 && i % 3 === 0) {
+    console.log('fizzbuzz')
+        }
+  else if (i % 3 === 0) {
+    console.log('fizz')
+  } else if (i % 5 === 0) {
+    console.log('buzz')
+  }
+  }
